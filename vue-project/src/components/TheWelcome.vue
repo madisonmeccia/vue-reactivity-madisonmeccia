221d636b03fb795ff6170642348f3ed81f2633e0
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-04-02 19:50:09 -0400

    n

--- vue-project/src/components/TheWelcome.vue
+++ vue-project/src/components/TheWelcome.vue
@@ -10,38 +10,41 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
-  </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
+    <h1 class="heading">MAKE UR OWN ACAI BOWL! :))</h1>
     <img
       class="blueberry"
       src="../assets/blueberry.png"
       width="90"
       height="90"
     />
-    <button v-on:click="redirect_to_login">
-      img (class="raz" src="../assets/raz.png" width="80" height="65")
-    </button>
+    <img class="strw" src="../assets/strw.png" width="110" height="70" />
     <img class="gg" src="../assets/gg.png" width="60" height="60" />
+    <img class="choc" src="../assets/choc.png" width="90" height="90" />
     <img class="drizzle" src="../assets/drizzle.webp" width="90" height="90" />
     <img class="coco" src="../assets/coco.png" width="90" height="90" />
     <img class="blueraz" src="../assets/blueraz.png" width="90" height="90" />
     <img class="pitaya" src="../assets/pitaya.png" width="90" height="90" />
+    <img class="greenz" src="../assets/green.png" width="90" height="90" />
     <img class="acai" src="../assets/acai.png" width="90" height="90" />
   </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its
-    sustainability. You can help us by
-  </WelcomeItem>
+  <p class="p">
+    choose ur toppings and watch ur acai bowl being made right on front of u!
+  </p>
 </template>
+<style scoped>
+.heading {
+  color: deeppink;
+  font-family: Trattatello, fantasy;
+  font-weight: bold;
+}
+.p {
+  color: black;
+  font-family: monospace;
+  font-weight: bold;
+  margin-left: 80px;
+  display: flex;
+  text-align: center;
+  font-size: large;
+}
+</style>
