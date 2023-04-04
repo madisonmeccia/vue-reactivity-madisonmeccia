f3e6d7ae4d9109614ccd2b505d70ea019eb9a37b
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-04-03 22:35:21 -0400

    f

--- vue-project/src/components/TheWelcome.vue
+++ vue-project/src/components/TheWelcome.vue
@@ -36,42 +36,42 @@ export default {
   ></div>
   <div class="allItems">
     <img
-      @click="addItem('../src/assets/blueberry.png')"
+      @click="addItem('../assets/blueberry.png')"
       class="blueberry"
       src="../assets/blueberry.png"
       width="110"
       height="90"
     />
     <img
-      @click="addItem('../src/assets/strw.png')"
+      @click="addItem('../assets/strw.png')"
       class="strw"
       src="../assets/strw.png"
       width="110"
       height="70"
     />
     <img
-      @click="addItem('../src/assets/gg.png')"
+      @click="addItem('../assets/gg.png')"
       class="gg"
       src="../assets/gg.png"
       width="60"
       height="60"
     />
     <img
-      @click="addItem('../src/assets/ban.png')"
+      @click="addItem('../assets/ban.png')"
       class="ban"
       src="../assets/ban.png"
       width="80"
       height="60"
     />
     <img
-      @click="addItem('../src/assets/choc.png')"
+      @click="addItem('../assets/choc.png')"
       class="choc"
       src="../assets/choc.png"
       width="90"
       height="80"
     />
     <img
-      @click="addItem('../src/assets/drizzle.webp')"
+      @click="addItem('../assets/drizzle.webp')"
       class="drizzle"
       src="../assets/drizzle.webp"
       width="90"
@@ -79,41 +79,41 @@ export default {
     />
 
     <img
-      @click="addItem('../src/assets/coco.png')"
+      @click="addItem('../assets/coco.png')"
       class="coco"
       src="../assets/coco.png"
       width="90"
       height="90"
     />
     <img
-      @click="addItem('../src/assets/blueraz.png')"
+      @click="addItem('../assets/blueraz.png')"
       class="blueraz"
       src="../assets/blueraz.png"
       width="95"
       height="90"
     />
     <img
-      @click="addItem('../src/assets/pitaya.png')"
+      @click="addItem('../assets/pitaya.png')"
       class="pitaya"
       src="../assets/pitaya.png"
       width="95"
       height="90"
     />
     <img
-      @click="addItem('../src/assets/acai.png')"
+      @click="addItem('../assets/acai.png')"
       class="acai"
       src="../assets/acai.png"
       width="90"
       height="90"
     />
     <img
-      @click="addItem('../src/assets/green.png')"
+      @click="addItem('../assets/green.png')"
       class="greenz"
       src="../assets/green.png"
       width="90"
       height="90"
     />
-    <button @click="addItem('../src/assets/bowl1.png')" class="clear">
+    <button @click="addItem('../assets/bowl1.png')" class="clear">
       restart order!
     </button>
   </div>
